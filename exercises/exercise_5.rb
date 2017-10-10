a389e8c130a940846b561e475f72a596ec1f0618
Deanna Lepke <deannalepke@Deannas-MacBook-Pro.local>
2017-10-10 14:20:13 -0700

    completed exercise 5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,13 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+@total_stores = Store.count
+@total_stores_over_1M = Store.where("annual_revenue > ?", 1000000)
+
+puts @total_revenue
+
+puts @total_revenue / @total_stores
+
+puts @total_stores_over_1M.count
